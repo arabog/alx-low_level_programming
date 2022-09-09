b6ade582ba4cc9643582a853e72173f614a73c5a
arabog <arababs2015@gmail.com>
2022-09-09 13:58:18 -0700

    add: 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,12 +6,12 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{       
+{
 char xter[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 int i;
 
 for (i = 0; i < 52; i++)
-{       
+{
 putchar(xter[i]);
 }
 putchar('\n');
