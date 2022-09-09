5bd3bb370ecab003c6fab5b01371f6758a0e85d9
arabog <arababs2015@gmail.com>
2022-09-09 14:27:17 -0700

    add: 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char xter[10] = "0123456789";
+int i;
+
+for (i = 0; i < 10; i++)
+{
+putchar(xter[i]);
+}
+putchar('\n');
+return (0);
+}
+
