c36752c8ba7f015c6cefbe44841e3212e5322bf0
arabog <arababs2015@gmail.com>
2022-09-09 14:35:22 -0700

    add: 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints the numbers.
+ * main - Prints numbers between 0 to 9.
  *
  * Return: Always 0 (Success)
  */
@@ -9,7 +9,7 @@ int main(void)
 {
 int i;
 
-for (i = 0; i < 10; i++)
+for (i = 48; i < 58; i++)
 {
 putchar(i);
 }
