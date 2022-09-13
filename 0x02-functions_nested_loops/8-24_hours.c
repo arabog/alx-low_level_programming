87e550fb39fae213ab8180e518826d046fa0917a
arabog <arababs2015@gmail.com>
2022-09-13 10:38:19 -0700

    add: 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,7 +11,7 @@ int j, k, l, m;
 
 for (j = 48; j <= 50; j++)
 {
-for (k = 48; k <= 57; k++)
+for (k = 48; k <= 53; k++)
 {
 for (l = 48; l <= 53; l++)
 {
@@ -20,13 +20,13 @@ for (m = 48; m <= 57; m++)
 if (j >= 50 && k >= 52)
 {
 break;
+}
 _putchar(j);
 _putchar(k);
 _putchar(58);
 _putchar(l);
 _putchar(m);
 _putchar('\n');
-}
 }
 }
 }
