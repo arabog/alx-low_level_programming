604496978e5e7e125df8145b2fd23b0fbe901eb8
arabog <arababs2015@gmail.com>
2022-09-13 11:20:15 -0700

    add: 9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,9 +6,9 @@
  * Return: no return
  */
 
-void time_table(void)
+void times_table(void)
 {
-int a, b, sign;
+int a, b, val;
 
 for (a = 0; a <= 9; a++)
 {
@@ -16,18 +16,18 @@ _putchar(48);
 
 for (b = 1; b <= 9; b++)
 {
-sign = a * b;
+val = a * b;
 _putchar(44); /* , */
 _putchar(32); /*space */
-if (sign <= 9)
+if (val <= 9)
 {
 _putchar(32);
-_putchar(sign + 48);
+_putchar(val + 48);
 }
 else
 {
-_putchar((sign / 10) + 48);
-_putchar((sign % 10) + 48);
+_putchar((val / 10) + 48);
+_putchar((val % 10) + 48);
 }
 }
 _putchar('\n');
