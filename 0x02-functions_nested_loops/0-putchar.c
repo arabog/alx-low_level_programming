77a2b465bf6f53fee5342f1e56b8cfda4f4b1437
arabog <arababs2015@gmail.com>
2022-09-13 06:24:53 -0700

    add: 0x02 directory, README.md 0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,15 @@
 #include "main.h"
-/*
+/**
  * main - print _putchar as a message
  *
  * Return: Always 0 (Success)
  *
  */
-int main(void);
 int main(void)
 {
 int  val[] = {95, 112, 117, 116, 99, 104, 97, 114};
 int inte, sz;
+
 sz = sizeof(val) / sizeof(int);
 for (inte = 0; inte  < sz; inte++)
 {
