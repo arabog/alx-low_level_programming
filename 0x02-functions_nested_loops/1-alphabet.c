5c2c1937a0980fb63dc4d60dbc1e6e3259dda631
arabog <arababs2015@gmail.com>
2022-09-13 07:19:04 -0700

    add: 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success).
+ */
+void print_alphabet(void)
+{
+int c;
+
+for (c = 97; c < 123; c++)
+{
+_putchar((c));
+}
+
+_putchar('\n');
+return (0);
+}
