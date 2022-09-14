612695c28cce34956925ce7778b082fc136f5717
arabog <arababs2015@gmail.com>
2022-09-14 13:16:31 -0700

    add: 0-main.c main.h

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,19 @@
 #include "main.h"
-
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
-
 int main(void)
 {
 int i;
-
 i = 98;
+if (i > 48)
+{
 positive_or_negative(i);
-
+}
+else
+{
+positive_or_negative(i);
+}
 return (0);
 }
