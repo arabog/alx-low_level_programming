a8de0ffba086e93d400de65c635f486e7e457d66
arabog <arababs2015@gmail.com>
2022-09-15 07:17:00 -0700

    add: 8-squares.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square -print square
+ *
+ * @size: size of the squaree
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_square(int size)
+{
+int i, j;
+
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar(35);
+}
+_putchar('\n');
+
+}
+
+_putchar('\n');
+}
