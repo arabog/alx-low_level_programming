d22bee24623ebc97e7917b70a4b3bcc6200bd539
arabog <arababs2015@gmail.com>
2022-09-15 07:53:10 -0700

    add: 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - fizzbuzz print number from 1 to 100
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i, num;
+num = 100;
+
+for (i = 1; i <= num; i++)
+{
+if (i % 15 == 0)
+{
+printf(" FizzBuzz");
+}
+else if (i % 5 == 0)
+{
+printf(" Buzz");
+}
+else if (i % 3 == 0)
+{
+printf(" Fizz");
+}
+else
+{
+printf(" %d\n", i);
+}
+
+
+}
+putchar('\n');
+return (0);
+}
