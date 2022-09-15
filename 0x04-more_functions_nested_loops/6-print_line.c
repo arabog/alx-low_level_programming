c109884de8227057de29abbfe93577a3350c3e20
arabog <arababs2015@gmail.com>
2022-09-15 05:18:44 -0700

    add: 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ *
+ * @n: numberof times to print _
+ * Return: Always 0 (Success)
+ */
+
+void print_line(int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+_putchar(95);
+}
+
+_putchar('\n');
+
+}
