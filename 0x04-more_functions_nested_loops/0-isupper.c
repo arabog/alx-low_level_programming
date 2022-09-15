beedd31e62603f5099abaf9b4d2626f8f5346bdb
arabog <arababs2015@gmail.com>
2022-09-15 02:00:40 -0700

    add: 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -10,5 +10,21 @@
 
 int _isupper(int c)
 {
+int a, b, d, e;
 
+a = 97;
+b = 122;
+d = 65;
+e = 90;
+
+if (c >= a && c <= b)
+{
+return (0);
+}
+else if (c >= d && c <= e)
+{
+return (1);
+}
+
+return (0);
 }
