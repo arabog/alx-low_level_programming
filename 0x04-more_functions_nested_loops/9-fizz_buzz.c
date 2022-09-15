9d0946eb65452b6354a576adb020829e280a37c3
arabog <arababs2015@gmail.com>
2022-09-15 08:19:18 -0700

    add: 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,31 +6,30 @@
  * Return: Always 0 (Success)
  */
 
+
 int main(void)
 {
 int i, num;
 num = 100;
 
-
-for (i = 1; i <= num; i++)
+for (i = 2; i <= num; i++)
 {
 if ((i % 3 == 0) && (i % 5 == 0))
 {
 printf(" FizzBuzz");
 }
-else if (i % 5 == 0)
-{
-printf(" Buzz");
-}
 else if (i % 3 == 0)
 {
 printf(" Fizz");
 }
+else if (i % 5 == 0)
+{
+printf(" Buzz");
+}
 else
 {
-printf(" %d\n", i);
+printf(" %d", i);
 }
-
 }
 
 printf("\n");
