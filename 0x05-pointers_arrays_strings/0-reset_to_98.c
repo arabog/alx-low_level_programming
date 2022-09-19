282ad65815114817bc7675dd7b618e23b8cccc53
arabog <arababs2015@gmail.com>
2022-09-19 11:07:09 -0700

    add: README.md, 0-main.c, 0-reset_to_98.c, main.h

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - update the value of  parameter
+ * @n: parameter to be updated
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef _main_h_
+#define _main_h_
+/*
+* This contains function protototypes
+* These are use to test the functions
+*
+*/
+
+void reset_to_98(int *n);
+
+
+#endif
