133039b09182f6c4f3892c84db81b4d3406166dd
arabog <arababs2015@gmail.com>
2022-10-06 06:40:28 -0700

    modify: 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "holberton.h"
+#include "main.h"
 
 /**
  * is_digit - checks if a string contains a non-digit char
@@ -8,6 +8,7 @@
  *
  * Return: 0 if a non-digit is found, 1 otherwise
  */
+
 int is_digit(char *s)
 {
 	int i = 0;
@@ -18,6 +19,7 @@ int is_digit(char *s)
 			return (0);
 		i++;
 	}
+
 	return (1);
 }
 
@@ -27,6 +29,7 @@ int is_digit(char *s)
  *
  * Return: the length of the string
  */
+
 int _strlen(char *s)
 {
 	int i = 0;
@@ -35,12 +38,14 @@ int _strlen(char *s)
 	{
 		i++;
 	}
+
 	return (i);
 }
 
 /**
  * errors - handles errors for main
  */
+
 void errors(void)
 {
 	printf("Error\n");
@@ -54,22 +59,29 @@ void errors(void)
  *
  * Return: always 0 (Success)
  */
+
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
+
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
+
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
+
 	result = malloc(sizeof(int) * len);
+
 	if (!result)
 		return (1);
+
 	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
+
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
@@ -81,19 +93,26 @@ int main(int argc, char *argv[])
 			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
 		}
+
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
 	}
+
 	for (i = 0; i < len - 1; i++)
 	{
 		if (result[i])
 			a = 1;
+
 		if (a)
 			_putchar(result[i] + '0');
 	}
+
 	if (!a)
 		_putchar('0');
+
 	_putchar('\n');
+
 	free(result);
+
 	return (0);
 }
