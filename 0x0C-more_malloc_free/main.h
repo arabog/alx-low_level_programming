14397c31221bf00ba75d12657b3573ad8871e76b
arabog <arababs2015@gmail.com>
2022-10-06 15:48:40 +0100

    modify: main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,16 +1,11 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 
-int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-
-void _is_zero(char *argv[]);
-char *_initialize_array(char *ar, int lar);
-int _checknum(char *argv[], int n);
 
 #endif
 
