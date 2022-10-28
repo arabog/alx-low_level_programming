422c46dc5806a41d75755bac1bfd628e69df4cd6
arabog <arababs2015@gmail.com>
2022-10-28 14:20:10 -0700

    modify: 5-main.c 5-free_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+
+/**
+ * new_dog - initializes a variable of type struct dog.
+ * @d: struct dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Return: no return;
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+
