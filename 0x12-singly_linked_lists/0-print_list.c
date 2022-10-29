aa46e9c491f8542313661ff10344f582f7f25b15
arabog <arababs2015@gmail.com>
2022-10-29 16:31:42 +0100

    modify: 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,17 +7,23 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t num;
+size_t num;
 
-	num = 0;
-	while (h != NULL)
-	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		num++;
-	}
-	return (num);
+num = 0;
+while (h != NULL)
+{
+if (h->str == NULL)
+{
+printf("[%d] %s\n", 0, "(nil)");
+}
+else
+{
+printf("[%d] %s\n", h->len, h->str);
+}
+
+h = h->next;
+num++;
+}
+
+return (num);
 }
